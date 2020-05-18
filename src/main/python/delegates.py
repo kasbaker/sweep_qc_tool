5d263b512edaab0517be4bdcdf199c9b72d96c89
kasbaker <katherine.baker.a@gmail.com>
2020-05-18 13:57:28 -0700

    Added some commented out code for radio button delegate.

--- src/main/python/delegates.py
+++ src/main/python/delegates.py
@@ -79,3 +79,51 @@ class ComboBoxDelegate(QItemDelegate):
 
     def updateEditorGeometry(self, editor, option, index):
         editor.setGeometry(option.rect)
+
+
+# class RadioButtonDelegate(QItemDelegate):
+#     def __init__(self, owner, choices):
+#         super().__init__(owner)
+#         self.items = choices
+#
+#     def createEditor(self, parent, option, index):
+#         editor = QComboBox(parent)
+#         editor.addItems(self.items)
+#         editor.activated.connect(self.onActivated)
+#
+#         return editor
+#
+#     def paint(self, painter, option, index):
+#         value = index.data(QtCore.Qt.DisplayRole)
+#         style = QApplication.style()
+#         opt = QStyleOptionComboBox()
+#         opt.text = str(value)
+#         opt.rect = option.rect
+#         style.drawComplexControl(QStyle.CC_ComboBox, opt, painter)
+#         QItemDelegate.paint(self, painter, option, index)
+#
+#     def onActivated(self):
+#         """ Triggered when the user makes a selection. When that occurs, focus
+#         is removed from the editing combobox, which in turn causes
+#         setModelData to be called on this delegate.
+#         """
+#
+#         app = QApplication.instance()
+#
+#         sender = app.sender()
+#         focus = app.focusWidget()
+#
+#         if sender is focus:
+#             focus.clearFocus()
+#
+#     def setEditorData(self, editor, index):
+#         value = index.data(QtCore.Qt.DisplayRole)
+#         num = self.items.index(value)
+#         editor.setCurrentIndex(num)
+#
+#     def setModelData(self, editor, model, index):
+#         value = editor.currentText()
+#         model.setData(index, value, QtCore.Qt.EditRole)
+#
+#     def updateEditorGeometry(self, editor, option, index):
+#         editor.setGeometry(option.rect)
