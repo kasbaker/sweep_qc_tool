36f8bb1294ddb9a78f9e945afcb2984c5edececb
kasbaker <katherine.baker.a@gmail.com>
2020-07-10 14:12:00 -0700

    Added scipy.special.cython_special to hiddenimports in hook-scipy.py

--- pyinstaller_hooks/hook-scipy.py
+++ pyinstaller_hooks/hook-scipy.py
@@ -1,8 +1,9 @@
 import os
-
 import scipy
 
 scipy_dir = os.path.dirname(scipy.__file__)
 scipy_dll_glob = os.path.join(scipy_dir, ".libs", "*.dll")
 
 binaries = [(scipy_dll_glob, "dlls")]
+
+hiddenimports = ["scipy.special.cython_special"]
