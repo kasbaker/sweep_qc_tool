163478bdf0170e5b209a73b8cb93861d50b341e7
kasbaker <katherine.baker.a@gmail.com>
2020-07-13 14:09:27 -0700

    Programmed tri processing testing, quad is broken now. Debugging.

--- src/benchmarks/multiprocess_plots.py
+++ src/benchmarks/multiprocess_plots.py
@@ -4,8 +4,9 @@ from pathlib import Path
 import multiprocessing as mp
 from copy import deepcopy
 import datetime as dt
-from json import load
+import json
 from warnings import filterwarnings
+from timeit import default_timer
 
 # from concurrent.futures import ProcessPoolExecutor
 from PyQt5.QtCore import QByteArray #QRectF
@@ -45,10 +46,19 @@ import pstats
 filterwarnings('ignore')
 
 with open(DEFAULT_QC_CRITERIA_FILE, "r") as path:
-    QC_CRITERIA = load(path)
+    QC_CRITERIA = json.load(path)
 
 with open(StimulusOntology.DEFAULT_STIMULUS_ONTOLOGY_FILE, "r") as path:
-    STIMULUS_ONTOLOGY = StimulusOntology(load(path))
+    STIMULUS_ONTOLOGY = StimulusOntology(json.load(path))
+
+
+def timer(function):
+    def new_function():
+        start_time = default_timer()
+        function()
+        elapsed = default_timer() - start_time
+        print('Function "{name}" took {time} seconds to complete.'.format(name=function.__name__, time=elapsed))
+    return new_function()
 
 
 def svg_from_mpl_axes(fig) -> QByteArray:
@@ -168,10 +178,15 @@ def run_experiment_qc(cell_qc_pipe: mp.Pipe, sweep_qc_pipe: mp.Pipe, qc_pipe: mp
     cell_qc_output.close()  # close other end of cell_qc pipe
     cell_features, cell_tags = cell_qc_input.recv()  # receive cell qc features
 
+    print(cell_features)
+    print(cell_tags)
+
     # unpack sweep qc pipe
     sweep_qc_input, sweep_qc_output = sweep_qc_pipe
     sweep_qc_output.close()  # close other end of sweep_qc pipe
     sweep_features = sweep_qc_input.recv()  # receive sweep qc features
+
+    print(sweep_features)
 
     cell_state, sweep_states = qc_experiment(
         ontology=STIMULUS_ONTOLOGY,
@@ -190,6 +205,8 @@ def run_experiment_qc(cell_qc_pipe: mp.Pipe, sweep_qc_pipe: mp.Pipe, qc_pipe: mp
     qc_results = (
         cell_features, cell_tags, sweep_features, cell_state, sweep_states
     )
+
+    print(qc_results)
 
     _, qc_out = qc_pipe
     qc_out.send(qc_results)
@@ -220,157 +237,296 @@ def make_plots(nwb_file: str, thumb_pipe: mp.Pipe = None, sweep_datas=None):
         return thumbs
 
 
+def single_process(nwb_file: str):
+    """ Does Auto QC and makes plots using single process.
+
+    Parameters:
+        nwb_file (str): string of nwb path
+
+    Returns:
+        thumbs (list[QByteArray]): a list of plot thumbnails
+
+        qc_results (tuple): cell_features, cell_tags,
+            sweep_features, cell_state, sweep_states
+
+    """
+    data_set = create_ephys_data_set(nwb_file=nwb_file)
+
+    # cell QC worker
+    cell_features, cell_tags = cell_qc_features(data_set)
+    cell_features = deepcopy(cell_features)
+
+    # sweep QC worker
+    sweep_features = sweep_qc_features(data_set)
+    sweep_features = deepcopy(sweep_features)
+    drop_tagged_sweeps(sweep_features)
+
+    # experiment QC worker
+    cell_state, sweep_states = qc_experiment(
+        ontology=STIMULUS_ONTOLOGY,
+        cell_features=cell_features,
+        sweep_features=sweep_features,
+        qc_criteria=QC_CRITERIA
+    )
+
+    qc_summary(
+        sweep_features=sweep_features,
+        sweep_states=sweep_states,
+        cell_features=cell_features,
+        cell_state=cell_state
+    )
+
+    qc_results = (
+        cell_features, cell_tags, sweep_features, cell_state, sweep_states
+    )
+
+    sweep_datas = list(map(
+        data_set.get_sweep_data,
+        list(range(len(data_set._data.sweep_numbers)))
+    ))  # grab sweep numbers from ._data.sweep_numbers (impolite, but fast)
+
+    thumbs = make_plots(nwb_file=nwb_file, sweep_datas=sweep_datas)
+
+    return thumbs, qc_results
+
+
+def plot_process(nwb_file: str):
+    """ Makes plots using single process.
+
+    Parameters:
+        nwb_file (str): string of nwb path
+
+    Returns:
+        thumbs (list[QByteArray]): a list of plot thumbnails
+
+    """
+    plot_pipe = mp.Pipe(duplex=False)
+    plot_worker = mp.Process(
+        name="plot_worker", target=make_plots, args=(nwb_file, plot_pipe)
+    )
+    return plot_pipe, plot_worker
+
+
+def dual_process(nwb_file: str):
+    """ Does Auto QC and makes plot using two processes.
+
+    Parameters:
+        nwb_file (str): string of nwb path
+
+    Returns:
+        thumbs (list[QByteArray]): a list of plot thumbnails
+
+        qc_results (tuple): cell_features, cell_tags,
+            sweep_features, cell_state, sweep_states
+
+    """
+
+    # spawn qc pipe and worker
+    # pipe for qc data
+    qc_pipe = mp.Pipe(duplex=False)
+    # worker to do auto-qc
+    qc_worker = mp.Process(
+        name="qc_worker",
+        target=run_auto_qc, args=(nwb_file, qc_pipe)
+    )
+
+    # spawn plot pipe and worker
+    plot_pipe, plot_worker = plot_process(nwb_file=nwb_file)
+
+    # start workers
+    qc_worker.start()
+    plot_worker.start()
+
+    # close pipes
+    qc_pipe[1].close()
+    plot_pipe[1].close()
+
+    # receive datas
+    thumbs = plot_pipe[0].recv()
+    qc_results = qc_pipe[0].recv()
+
+    # join workers
+    qc_worker.join()
+    plot_worker.join()
+
+    # kill workers
+    qc_worker.terminate()
+    plot_worker.terminate()
+
+    return thumbs, qc_results
+
+
+def tri_process(nwb_file: str):
+    """ Does Auto QC and makes plot using three processes.
+
+    Parameters:
+        nwb_file (str): string of nwb path
+
+    Returns:
+        thumbs (list[QByteArray]): a list of plot thumbnails
+
+        qc_results (tuple): cell_features, cell_tags,
+            sweep_features, cell_state, sweep_states
+
+    """
+    # pipe and worker for sweep qc data
+    sweep_qc_pipe = mp.Pipe(duplex=False)
+    sweep_qc_worker = mp.Process(
+        name="sweep_qc_worker",
+        target=run_sweep_qc, args=(nwb_file, sweep_qc_pipe)
+    )
+
+    # pipe and worker for cell qc data
+    cell_qc_pipe = mp.Pipe(duplex=False)
+    cell_qc_worker = mp.Process(
+        name="cell_qc_worker",
+        target=run_cell_qc, args=(nwb_file, cell_qc_pipe)
+    )
+
+    # pipe and worker for plot data
+    plot_pipe, plot_worker = plot_process(nwb_file)
+
+    # start workers
+    cell_qc_worker.start()
+    sweep_qc_worker.start()
+    plot_worker.start()
+
+    # close pipes
+    sweep_qc_pipe[1].close()
+    cell_qc_pipe[1].close()
+    plot_pipe[1].close()
+
+    # receive datas
+    thumbs = plot_pipe[0].recv()
+    sweep_features = sweep_qc_pipe[0].recv()
+    cell_features, cell_tags = cell_qc_pipe[0].recv()
+
+    cell_state, sweep_states = qc_experiment(
+        ontology=STIMULUS_ONTOLOGY,
+        cell_features=cell_features,
+        sweep_features=sweep_features,
+        qc_criteria=QC_CRITERIA
+    )
+
+    qc_summary(
+        sweep_features=sweep_features,
+        sweep_states=sweep_states,
+        cell_features=cell_features,
+        cell_state=cell_state
+    )
+
+    qc_results = (
+        cell_features, cell_tags, sweep_features, cell_state, sweep_states
+    )
+
+    # join workers
+    cell_qc_worker.join()
+    sweep_qc_worker.join()
+    plot_worker.join()
+
+    # kill workers
+    cell_qc_worker.terminate()
+    sweep_qc_worker.terminate()
+    plot_worker.terminate()
+
+    return thumbs, qc_results
+
+
+def quad_process(nwb_file: str):
+    """ Does Auto QC and makes plot using four processes.
+
+    Parameters:
+        nwb_file (str): string of nwb path
+
+    Returns:
+        thumbs (list[QByteArray]): a list of plot thumbnails
+
+        qc_results (tuple): cell_features, cell_tags,
+            sweep_features, cell_state, sweep_states
+
+    """
+
+    # pipe and worker for sweep qc data
+    sweep_qc_pipe = mp.Pipe(duplex=False)
+    sweep_qc_worker = mp.Process(
+        name="sweep_qc_worker",
+        target=run_sweep_qc, args=(nwb_file, sweep_qc_pipe)
+    )
+
+    # pipe and worker for cell qc data
+    cell_qc_pipe = mp.Pipe(duplex=False)
+    cell_qc_worker = mp.Process(
+        name="cell_qc_worker",
+        target=run_cell_qc, args=(nwb_file, cell_qc_pipe)
+    )
+
+    # pipe and worker for experiment qc
+    experiment_qc_pipe = mp.Pipe(duplex=False)
+    experiment_qc_worker = mp.Process(
+        name="experiment_qc_worker",
+        target=run_experiment_qc, args=(cell_qc_pipe, sweep_qc_pipe, cell_qc_pipe)
+    )
+
+    # pipe and worker for plot data
+    plot_pipe, plot_worker = plot_process(nwb_file)
+
+    # start workers
+    cell_qc_worker.start()
+    sweep_qc_worker.start()
+    experiment_qc_worker.start()
+    plot_worker.start()
+
+    # close pipes
+    sweep_qc_pipe[1].close()
+    cell_qc_pipe[1].close()
+    experiment_qc_pipe[1].close()
+    plot_pipe[1].close()
+
+    # receive data
+    qc_results = experiment_qc_pipe[0].recv()
+    thumbs = plot_pipe[0].recv()
+
+    # join workers
+    cell_qc_worker.join()
+    sweep_qc_worker.join()
+    experiment_qc_worker.join()
+    plot_worker.join()
+
+    # kill workers
+    cell_qc_worker.terminate()
+    sweep_qc_worker.terminate()
+    experiment_qc_worker.terminate()
+    plot_worker.terminate()
+
+    return thumbs, qc_results
+
+
 def main(nwb_file, dual=False, tri=False, quad=False):
-    # multiprocessing == True if dual or quad == True
-    multi = dual or tri or quad
-    # do this if we are using multiprocessing
-    if multi:
+    start_time = default_timer()
 
-        if dual:
-            # pipe for qc data
-            qc_pipe = mp.Pipe(duplex=False)
-            # worker to do auto-qc
-            qc_worker = mp.Process(
-                name="qc_worker",
-                target=run_auto_qc, args=(nwb_file, qc_pipe)
-            )
-            qc_worker.start()   # start qc worker
-
-            qc_pipe[1].close()
-
-            try:
-                qc_results = qc_pipe[0].recv()
-                for result in qc_results:
-                    print(result)
-            except EOFError:
-                print("error no qc data")
-
-        if tri:
-            # pipe and worker for sweep qc data
-            sweep_qc_pipe = mp.Pipe(duplex=False)
-            sweep_qc_worker = mp.Process(
-                name="sweep_qc_worker",
-                target=run_sweep_qc, args=(nwb_file, sweep_qc_pipe)
-            )
-            sweep_qc_worker.start()  # start sweep qc worker
-
-            # pipe and worker for cell qc data
-            cell_qc_pipe = mp.Pipe(duplex=False)
-            cell_qc_worker = mp.Process(
-                name="cell_qc_worker",
-                target=run_cell_qc, args=(nwb_file, cell_qc_pipe)
-            )
-            cell_qc_worker.start()  # start cell qc worker
-
-            # close sweep and cell qc pipes
-            sweep_qc_pipe[1].close()
-            cell_qc_pipe[1].close()
-
-        if quad:
-            # pipe for qc data
-            experiment_qc_pipe = mp.Pipe(duplex=False)
-            # pipe and worker for sweep qc data
-            sweep_qc_pipe = mp.Pipe(duplex=False)
-            sweep_qc_worker = mp.Process(
-                name="sweep_qc_worker",
-                target=run_sweep_qc, args=(nwb_file, sweep_qc_pipe)
-            )
-            sweep_qc_worker.start()  # start sweep qc worker
-
-            # pipe and worker for cell qc data
-            cell_qc_pipe = mp.Pipe(duplex=False)
-            cell_qc_worker = mp.Process(
-                name="cell_qc_worker",
-                target=run_cell_qc, args=(nwb_file, cell_qc_pipe)
-            )
-            cell_qc_worker.start()  # start cell qc worker
-
-            # worker to process qc results
-            experiment_qc_worker = mp.Process(
-                name="experiment_qc_worker",
-                target=run_experiment_qc, args=(cell_qc_pipe, sweep_qc_pipe, qc_pipe)
-            )
-            experiment_qc_worker.start()    # start experiment qc worker
-
-            # close sweep and cell qc pipes
-            sweep_qc_pipe[1].close()
-            cell_qc_pipe[1].close()
-            experiment_qc_pipe[1].close()
-            try:
-                qc_results = experiment_qc_pipe[0].recv()
-                for result in qc_results:
-                    print(result)
-            except EOFError:
-                print("error no qc data")
-
-        # pipe and worker for plot data
-        plot_pipe = mp.Pipe(duplex=False)
-        plot_worker = mp.Process(
-            name="plot_worker", target=make_plots, args=(nwb_file, plot_pipe)
-        )
-        plot_worker.start()  # start plot worker
-
-        # close plot pipe
-
-        # close qc pipe
-
-        try:
-            thumbnails = plot_pipe[0].recv()
-        except EOFError:
-            print("error no plot data")
-
-
-        # join and terminate workers
-        plot_worker.join()
-        if dual:
-            qc_worker.join()
-            qc_worker.terminate()
-        if quad:
-            sweep_qc_worker.join()
-            experiment_qc_worker.join()
-            sweep_qc_worker.terminate()
-            experiment_qc_worker.terminate()
-        plot_worker.terminate()
-
+    if dual:
+        thumbs, qc_results = dual_process(nwb_file=nwb_file)
+        elapsed_time = default_timer() - start_time
+        print(f"Dual processing of {nwb_file} took {elapsed_time} to complete")
+    elif tri:
+        thumbs, qc_results = tri_process(nwb_file=nwb_file)
+        elapsed_time = default_timer() - start_time
+        print(f"Tri processing of {nwb_file} took {elapsed_time} to complete")
+    elif quad:
+        thumbs, qc_results = quad_process(nwb_file=nwb_file)
+        elapsed_time = default_timer() - start_time
+        print(f"Quad processing of {nwb_file} took {elapsed_time} to complete")
     else:
-        data_set = create_ephys_data_set(nwb_file=nwb_file)
+        thumbs, qc_results = single_process(nwb_file=nwb_file)
+        elapsed_time = default_timer() - start_time
+        print(f"Single processing of {nwb_file} took {elapsed_time} to complete")
 
-        # cell QC worker
-        cell_features, cell_tags = cell_qc_features(data_set)
-        cell_features = deepcopy(cell_features)
+    # for thumb in thumbs:
+    #     print(thumb)
+    # for result in qc_results:
+    #     print(result)
 
-        # sweep QC worker
-        sweep_features = sweep_qc_features(data_set)
-        sweep_features = deepcopy(sweep_features)
-        drop_tagged_sweeps(sweep_features)
-
-        # experiment QC worker
-        cell_state, sweep_states = qc_experiment(
-            ontology=STIMULUS_ONTOLOGY,
-            cell_features=cell_features,
-            sweep_features=sweep_features,
-            qc_criteria=QC_CRITERIA
-        )
-
-        qc_summary(
-            sweep_features=sweep_features,
-            sweep_states=sweep_states,
-            cell_features=cell_features,
-            cell_state=cell_state
-        )
-
-        qc_results = (
-            cell_features, cell_tags, sweep_features, cell_state, sweep_states
-        )
-
-        sweep_datas = list(map(
-            data_set.get_sweep_data,
-            list(range(len(data_set._data.sweep_numbers)))
-        ))  # grab sweep numbers from ._data.sweep_numbers (impolite, but fast)
-
-        thumbs = make_plots(nwb_file=nwb_file, sweep_datas=sweep_datas)
-        for result in qc_results:
-            print(result)
+    return elapsed_time
 
 
 if __name__ == '__main__':
@@ -380,28 +536,37 @@ if __name__ == '__main__':
     today = dt.datetime.now().strftime('%y%m%d')
     now = dt.datetime.now().strftime('%H.%M.%S')
 
-    profile_dir = base_dir.joinpath(f'profiles/{today}/{now}/')
-    profile_dir.mkdir(parents=True)
+    # profile_dir = base_dir.joinpath(f'profiles/{today}/{now}/')
+    # profile_dir.mkdir(parents=True)
 
-    for index in range(len(files)):
+    time_file = base_dir.joinpath(f'process_times/{today}{now}.json')
+
+    times = {files[x]: dict.fromkeys(['mono', 'dual', 'tri', 'quad']) for x in range(len(files))}
+
+    for index in range(1):#range(len(files)):
         nwb_file = str(base_dir.joinpath(f'data/nwb/{files[index]}'))
 
-        # main(nwb_file=nwb_file, quad=True)
+        # times[files[index]]['mono'] = main(nwb_file=nwb_file)
+        # times[files[index]]['dual'] = main(nwb_file=nwb_file, dual=True)
+        # times[files[index]]['tri'] = main(nwb_file=nwb_file, tri=True)
+        times[files[index]]['quad'] = main(nwb_file=nwb_file, quad=True)
 
-        # benchmark dual processing
-        dual_profile_file = str(profile_dir.joinpath(f'dual_{files[index][0:-4]}.prof'))
-        cProfile.run('main(nwb_file, dual=True)', filename=dual_profile_file)
-        p = pstats.Stats(dual_profile_file)
-        p.sort_stats('cumtime').print_stats(2)
+        # main(nwb_file=nwb_file, tri=True)
 
-        # benchmark quad processing
-        quad_profile_file = str(profile_dir.joinpath(f'quad_{files[index][0:-4]}.prof'))
-        cProfile.run(
-            f'main(nwb_file, quad=True)',
-            filename=quad_profile_file
-        )
-        p = pstats.Stats(quad_profile_file)
-        p.sort_stats('cumtime').print_stats(2)
+        # # benchmark dual processing
+        # dual_profile_file = str(profile_dir.joinpath(f'dual_{files[index][0:-4]}.prof'))
+        # cProfile.run('main(nwb_file, dual=True)', filename=dual_profile_file)
+        # p = pstats.Stats(dual_profile_file)
+        # p.sort_stats('cumtime').print_stats(2)
+        #
+        # # benchmark quad processing
+        # quad_profile_file = str(profile_dir.joinpath(f'quad_{files[index][0:-4]}.prof'))
+        # cProfile.run(
+        #     f'main(nwb_file, quad=True)',
+        #     filename=quad_profile_file
+        # )
+        # p = pstats.Stats(quad_profile_file)
+        # p.sort_stats('cumtime').print_stats(2)
 
         # # benchmark single processing
         # profile_file = str(profile_dir.joinpath(f'single_{files[index][0:-4]}.prof'))
@@ -416,3 +581,13 @@ if __name__ == '__main__':
         # )
         # p = pstats.Stats(multi_profile_file)
         # p.sort_stats('cumtime').print_stats(20)
+
+    for file in times:
+        print(f"Elapsed times for {file}")
+        print(f"    Mono: {times[file]['mono']}")
+        print(f"    Dual: {times[file]['dual']}")
+        print(f"    Tri:  {times[file]['tri']}")
+        print(f"    Quad: {times[file]['quad']}")
+
+    with open(time_file, 'w') as file:
+        json.dump(times, file, indent=4)
