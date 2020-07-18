a083c8100563a5b445b9524c4d9592a94554a963
kasbaker <katherine.baker.a@gmail.com>
2020-07-18 00:54:13 -0700

    Got fast cel qc features working.

--- src/benchmarks/fast_qc.py
+++ src/benchmarks/fast_qc.py
@@ -5,7 +5,9 @@ import datetime as dt
 from multiprocessing import Pool, cpu_count
 import cProfile
 import pstats
+import logging
 
+import numpy as np
 from pynwb.icephys import (
     CurrentClampSeries, CurrentClampStimulusSeries,
     VoltageClampSeries, VoltageClampStimulusSeries,
@@ -16,37 +18,148 @@ from ipfx.qc_feature_extractor import cell_qc_features, sweep_qc_features
 from ipfx.qc_feature_evaluator import qc_experiment, DEFAULT_QC_CRITERIA_FILE
 from ipfx.dataset.create import create_ephys_data_set
 import ipfx.epochs as ep
+import ipfx.qc_features as qcf
+from ipfx import error as er
+from ipfx.qc_feature_extractor \
+    import extract_recording_date, compute_input_access_resistance_ratio
+
+
+def fast_extract_blowout(blowout_sweeps, tags):
+    if blowout_sweeps:
+        blowout_mv = qcf.measure_blowout(
+            blowout_sweeps[-1]['response'], blowout_sweeps[-1]['test_epoch'][1]
+        )
+    else:
+        tags.append("Blowout is not available")
+        blowout_mv = None
+    return blowout_mv
+
+
+def fast_extract_electrode_0(bath_sweeps, tags):
+    if bath_sweeps:
+        e0 = qcf.measure_electrode_0(
+            bath_sweeps[-1]['response'], bath_sweeps[-1]['sampling_rate']
+        )
+    else:
+        tags.append("Electrode 0 is not available")
+        e0 = None
+    return e0
+
+
+def fast_extract_clamp_seal(seal_sweeps, tags):
+    if seal_sweeps:
+        num_pts = len(seal_sweeps[-1]['stimulus'])
+        time = np.linspace(0, num_pts/seal_sweeps[-1]['sampling_rate'], num_pts)
+        seal_gohm = qcf.measure_seal(
+            seal_sweeps[-1]['stimulus'], seal_sweeps[-1]['response'], time
+        )
+        if seal_gohm is None or not np.isfinite(seal_gohm):
+            raise er.FeatureError("Could not compute seal")
+    else:
+        tags.append("Seal is not available")
+        seal_gohm = None
+
+    return seal_gohm
+
+
+def fast_extract_input_and_acess_resistance(breakin_sweeps, tags):
+    if breakin_sweeps:
+        num_pts = len(breakin_sweeps[-1]['stimulus'])
+        time = np.linspace(0, num_pts/breakin_sweeps[-1]['sampling_rate'], num_pts)
+        try:
+            input_resistance = qcf.measure_input_resistance(
+                breakin_sweeps[-1]['stimulus'], breakin_sweeps[-1]['response'], time
+            )
+        except Exception as e:
+            logging.warning("Error reading input resistance.")
+            raise
+
+        try:
+            access_resistance = qcf.measure_initial_access_resistance(
+                breakin_sweeps[-1]['stimulus'], breakin_sweeps[-1]['response'], time
+            )
+        except Exception as e:
+            logging.warning("Error reading initial access resistance.")
+            raise
+
+    else:
+        tags.append("Breakin sweep not found")
+        input_resistance = None
+        access_resistance = None
+
+    return input_resistance, access_resistance
 
 
 def fast_experiment_qc(nwb_file):
     data_set = create_ephys_data_set(nwb_file=nwb_file)
-    ontology = data_set.ontology
 
     sweep_table = data_set._data.nwb.sweep_table
-    sweep_range = range(sweep_table.series.shape[0]//2)
+
+    # number of sweeps is half the shape of the sweep table here because
+    # each sweep has two series associated with it (stimulus and response)
+    num_sweeps = sweep_table.series.shape[0]//2
+
+    # reverse the sweep list so when we grab the last sweep when we iterate
+    # through to find the appropriate sweep
+    sweep_range = range(num_sweeps)
     series_iter = map(sweep_table.get_series, sweep_range)
 
+    # iterator with all the necessary sweep data
+    data_iter = map(extract_series_data, series_iter)
 
-    experiment_data = tuple(map(extract_series_data, series_iter))
+    blowout_sweeps, bath_sweeps, seal_sweeps, breakin_sweeps = get_sweep_types(
+        data_iter, data_set.ontology
+    )
 
-    # blowout_names = ontology.blowout_names
-    # bath_names = ontology.bath_names
-    #
+    features = {}
+    tags = []
+
+    features['blowout_mv'] = fast_extract_blowout(blowout_sweeps, tags)
+
+    features['electrode_0_pa'] = fast_extract_electrode_0(bath_sweeps, tags)
+
+    features['recording_date'] = extract_recording_date(data_set, tags)
+
+    features["seal_gohm"] = fast_extract_clamp_seal(seal_sweeps, tags)
+
+    input_resistance, access_resistance = \
+        fast_extract_input_and_acess_resistance(breakin_sweeps, tags)
+
+    features['input_resistance_mohm'] = input_resistance
+    features["initial_access_resistance_mohm"] = access_resistance
+
+    features['input_access_resistance_ratio'] = \
+        compute_input_access_resistance_ratio(input_resistance, access_resistance)
+
+    return features, tags
 
 
-    # for sweep in experiment_data:
-    #     print(sweep)
+def get_sweep_types(data_iter, ontology):
+
+    blowout_sweeps = []
+    bath_sweeps = []
+    seal_sweeps = []
+    breakin_sweeps = []
+
+    for sweep in data_iter:
+        stim_code = sweep['stimulus_code']
+        if ontology.blowout_names[0] in stim_code:
+            blowout_sweeps.append(sweep)
+        if ontology.bath_names[0] in stim_code:
+            bath_sweeps.append(sweep)
+        if ontology.seal_names[0] in stim_code:
+            seal_sweeps.append(sweep)
+        if ontology.breakin_names[0] in stim_code:
+            breakin_sweeps.append(sweep)
+
+    return blowout_sweeps, bath_sweeps, seal_sweeps, breakin_sweeps
 
 
 def extract_series_data(series):
 
-    # sweep_number = series[0].sweep_number
+    sweep_number = series[0].sweep_number
 
-    stim_code = series[0].stimulus_description
-    if stim_code[-5:] == "_DA_0":
-        stim_code = stim_code[:-5]
-
-    stimulus_code = stim_code.split("[")[0]
+    stimulus_code = ""
 
     response = None
     stimulus = None
@@ -56,6 +169,14 @@ def extract_series_data(series):
     for s in series:
         if isinstance(s, (VoltageClampSeries, CurrentClampSeries, IZeroClampSeries)):
             response = s.data[:] * float(s.conversion)
+
+            stim_code = s.stimulus_description
+            if stim_code[-5:] == "_DA_0":
+                stim_code = stim_code[:-5]
+
+            stimulus_code = stim_code.split("[")[0]
+            # TODO use EphysDataInterface.get_stimulus_name(stimulus_code) here
+
         elif isinstance(s, (VoltageClampStimulusSeries, CurrentClampStimulusSeries)):
             stimulus = s.data[:] * float(s.conversion)
             unit = s.unit
@@ -78,7 +199,7 @@ def extract_series_data(series):
     epochs = get_epochs(sampling_rate,stimulus,response)
 
     return {
-        # 'sweep_number': sweep_number,
+        'sweep_number': sweep_number,
         'stimulus_code': stimulus_code,
         'stimulus': stimulus,
         'response': response,
@@ -197,11 +318,18 @@ if __name__ == "__main__":
     profile_dir = base_dir.joinpath(f'fast_qc_profiles/{today}_{now}')
     # profile_dir.mkdir(parents=True)
 
+    # fast_experiment_qc(nwb_file=str(base_dir.joinpath(files[1])))
+
     for file in files:
         start_time = default_timer()
-        fast_experiment_qc(nwb_file=str(base_dir.joinpath(file)))
+        features, tags = fast_experiment_qc(nwb_file=str(base_dir.joinpath(file)))
+
         print(f'{file} took {default_timer()-start_time} to load')
-        
+        for feature in features.items():
+            print(feature)
+        print('--------------------------------------------------------------')
+
+
         # profile_file = str(profile_dir.joinpath(f'{file.stem}.prof'))
         # cProfile.run(
         #     'fast_experiment_qc(nwb_file=str(base_dir.joinpath(file)))',
